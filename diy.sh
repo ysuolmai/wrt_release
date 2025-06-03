ddb6615eb69d1f92f7c1722cf3178bd548951977
ysuolmai <fan0417@gmail.com>
2025-06-03 18:46:37 +0800

    Update diy.sh
    Signed-off-by: ysuolmai <fan0417@gmail.com>

--- diy.sh
+++ diy.sh
@@ -100,13 +100,13 @@ keywords_to_delete=(
 )
 
 
-#[[ $FIRMWARE_TAG == *"NOWIFI"* ]] && keywords_to_delete+=("wpa" "hostapd" "wifi" "wireless" "ath11k" "80211" "kmod-qcom-qmi-helpers")
+#[[ $FIRMWARE_TAG == *"NOWIFI"* ]] && keywords_to_delete+=("wpa" "hostapd" "wifi" "wireless")
 #[[ $FIRMWARE_TAG != *"EMMC"* ]] && keywords_to_delete+=("samba" "autosamba" "jdcloud_ax1800-pro" "redmi_ax5-jdcloud")
 #[[ $FIRMWARE_TAG != *"EMMC"* ]] && keywords_to_delete+=("samba" "autosamba" "disk")
 #[[ $FIRMWARE_TAG == *"EMMC"* ]] && keywords_to_delete+=("cmiot_ax18" "qihoo_v6" "redmi_ax5=y" "zn_m2")
 
 for keyword in "${keywords_to_delete[@]}"; do
-    sed -i "/$keyword/d" ./.config
+    sed -i "/$keyword/d" $BASE_PATH/deconfig/$Dev.config
 done
 
 
@@ -232,7 +232,7 @@ fi
 
 # Append configuration lines to .config
 for line in "${provided_config_lines[@]}"; do
-    echo "$line" >> .config
+    echo "$line" >> $BASE_PATH/deconfig/$Dev.config
 done
 
 #./scripts/feeds update -a
